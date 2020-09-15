41c5507c0eb6f46a176b9e043a2ec755d60af4a8
Dan Beers <spanyell@icloud.com>
2020-09-15 09:17:00 -0700

    Initial Commit

---
+++ Shared/ConversionsApp.swift
@@ -0,0 +1,17 @@
+//
+//  ConversionsApp.swift
+//  Shared
+//
+//  Created by Dan Beers on 9/15/20.
+//
+
+import SwiftUI
+
+@main
+struct ConversionsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
